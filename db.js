e96fafcbcfc5a021e07172999c937d2a4f71ed54
Msohail <msoha001@doc.gold.ac.uk>
2024-12-12 15:51:02 +0000

    tets

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Abdulhadi123',
-  database: process.env.DB_NAME || 'project_food',
-});
+//const connection = mysql.createConnection({
+ // host: process.env.DB_HOST || 'localhost',
+//  user: process.env.DB_USER || 'root',
+//  password: process.env.DB_PASSWORD || 'abcd',
+//  database: process.env.DB_NAME || 'project_food',
+//});
+
+const db = mysql.createConnection ({
+  host: 'localhost',
+  user: 'bettys_books_app',
+  password: 'qwertyuiop',
+  database: 'bettys_books'
+})
+
 
 connection.connect((err) => {
   if (err) throw err;
