b081b3afeb5dc5a534aa21e4484d60cba4cf63c1
Msohail <msoha001@doc.gold.ac.uk>
2024-12-21 16:50:01 +0000

    add test data

---
+++ insert_test_data.sql
@@ -0,0 +1,6 @@
+# Insert data into the tables
+
+project_food
+
+INSERT INTO FastFoods (id, title, content, price, stock, image )
+VALUES(1, 'Cheese Burger 1','A delicious beef patty with melted cheese, lettuce, tomato, and pickles','5.99', '150', 'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg');
