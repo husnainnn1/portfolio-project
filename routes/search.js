587f88d0cf02eb51bc4d81f753ebff82205a0c23
Msohail <msoha001@doc.gold.ac.uk>
2024-12-21 15:54:40 +0000

    test for search page

--- routes/search.js
+++ routes/search.js
@@ -34,7 +34,7 @@ var appData = {RestauramtName: "Speedy Bites"}
 */
 
 //search result for extension search-topics
-/*router.post('/', function (req, res) {
+router.post('/', function (req, res) {
   // Extract the query keyword from the POST body
   const keyword = req.body.query;
   //searching in the database
@@ -46,38 +46,9 @@ var appData = {RestauramtName: "Speedy Bites"}
           //if code doesn't run it'll run this 
           return res.redirect("./")
       }
-      const newData = Object.assign({}, appData, {existingTitle:result || []});
+      const newData = Object.assign({}, appData, {results});
       console.log(newData)
       res.render("search.ejs", newData)
-  });
-});*/
-
-router.post('/', function (req, res) {
-  const keyword = req.body.query; // Extract 'query' from POST body
-
-  // SQL query to search for fast food
-  const sqlquery = `SELECT * FROM FastFoods WHERE title LIKE '%${keyword}%'`;
-
-  db.query(sqlquery, (err, result) => {
-    if (err) {
-      console.error("Database query error:", err);
-      return res.redirect("/"); // Redirect on error
-    }
-
-    console.log("Database query result:", result); // Log query results for debugging
-
-    // Pass 'existingTitle' to the EJS template
-    const newData = {
-      ...appData, // Include app-specific data
-      existingTitle: result || [] // Ensure result is an array
-    };
-    res.render("search.ejs", {
-      existingTitle: [
-        { title: 'Pizza', content: 'Cheesy pizza', price: 10, stock: 5, image: 'pizza.jpg' },
-        { title: 'Burger', content: 'Juicy burger', price: 8, stock: 3, image: 'burger.jpg' }
-      ]
-    });
-    res.render("search.ejs", newData); // Render EJS with newData
   });
 });
 
