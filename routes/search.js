5a3c305b8429d82eb8a46f66e9d9058dd21ecbb2
Msohail <msoha001@doc.gold.ac.uk>
2024-12-21 15:42:13 +0000

    fix

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('../db'); // Assuming you have a database connection file
 const router = express.Router();
 const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var appData = {RestauramtName: "Speedy Bites"}
